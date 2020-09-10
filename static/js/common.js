ebba389b793abc5ee2c812417c358ec7f7e935d3
cscs <>
2020-09-10 14:56:48 +0800

    提交

---
+++ static/js/common.js
@@ -0,0 +1,11 @@
+window.onload = window.onresize = function () {
+  var view_width = document.getElementsByTagName('html')[0].getBoundingClientRect().width;
+  //找到标签名为html的第一个
+  var _html = document.getElementsByTagName('html')[0];
+  //如果屏幕宽度大于640 
+  //就用 640/16=40
+  //小于640的时候 不确定 也是尺寸/40
+  view_width > 750 ? _html.style.fontSize = 750 / 16 + 'px' : _html.style.fontSize = view_width / 16 + 'px';
+  //最大网页尺寸640 
+  //最大字体（根字体大小） 40
+}

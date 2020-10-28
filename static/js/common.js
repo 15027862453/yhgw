cbfa2de36389f9f85f9801dc75060fb4c0d3c8a8
蔡崇 <7735913+cai_chong@user.noreply.gitee.com>
2020-10-28 12:22:22 +0800

    111

--- static/js/common.js
+++ static/js/common.js
@@ -5,7 +5,7 @@ window.onload = window.onresize = function () {
   //如果屏幕宽度大于640 
   //就用 640/16=40
   //小于640的时候 不确定 也是尺寸/40
-  view_width > 750 ? _html.style.fontSize = 750 / 16 + 'px' : _html.style.fontSize = view_width / 16 + 'px';
-  //最大网页尺寸640 
-  //最大字体（根字体大小） 40
+  view_width > 640 ? _html.style.fontSize = 640 / 16 + 'px' : _html.style.fontSize = view_width / 16 + 'px';
+		//最大网页尺寸640
+    //最大字体（根字体大小） 
 }
